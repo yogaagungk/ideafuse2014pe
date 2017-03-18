71c3c6d902bd65fd302617f83fb1d414d988896c
yogaagungk <yogaagungk@gmail.com>
2017-03-18 22:16:10 +0700

    Penyisihan Ideafuse 2014

---
+++ A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int N, P;
+	scanf("%d", &N);
+	for(int i=0;i<N;i++){
+		scanf("%d", &P);
+		printf("k");
+		for(int j=0;j<P;j++){
+			printf("a");
+		}
+		for(int j=0;j<2;j++){
+			printf("m");
+			for(int k=0;k<P;k++){
+				printf("e");
+			}
+			printf("h");
+			for(int k=0;k<P;k++){
+				printf("a");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
